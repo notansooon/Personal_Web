886afa497a544cc0d862b47d6bf16a8445483228
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-06-19 18:58:37 +0530

    UI change

--- src/Components/Pages/Apply.jsx
+++ src/Components/Pages/Apply.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import React from "react";
+import { GoogleGeminiEffect } from "/Users/aryanpalit/WebstormProjects/dlqf_website/src/Components/UI/google-gemini-effect.tsx";
 
-const Apply = () => {
+export function GoogleGeminiEffectDemo() {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
   return (
-    <div>Apply</div>
-  )
+      <div
+          className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
+          ref={ref}
+      >
+        <GoogleGeminiEffect
+            pathLengths={[
+              pathLengthFirst,
+              pathLengthSecond,
+              pathLengthThird,
+              pathLengthFourth,
+              pathLengthFifth,
+            ]}
+        />
+      </div>
+  );
 }
-
-export default Apply
