e536655c55ebe6f6f03d7e04448cfd5b9586c9be
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-06-21 00:26:25 +0530

    Update Home.jsx

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -68,7 +68,7 @@ const Home = () => {
                 <div className="tradingview-widget-container__widget"></div>
                 <div className="tradingview-widget-copyright">
                     <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-                        <span className="blue-text">Track all markets on TradingView</span>
+                       
                     </a>
                 </div>
             </div>
