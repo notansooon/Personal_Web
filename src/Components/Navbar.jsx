489929bcd893257d2c9151d2d4c34fdcc0996713
notansooon <ansonjiang043@gmail.com>
2024-06-20 16:12:33 -0400

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,58 +3,11 @@ import React, { useEffect, useRef } from 'react';
 
 
 const Navbar = () => {
-    const widgetContainerRef = useRef(null);
-   
-
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
-        script.async = true;
-        script.innerHTML = JSON.stringify({
-            symbols: [
-                {
-                    proName: "FOREXCOM:SPXUSD",
-                    title: "S&P 500 Index"
-                },
-                {
-                    proName: "FOREXCOM:NSXUSD",
-                    title: "US 100 Cash CFD"
-                },
-                {
-                    proName: "FX_IDC:EURUSD",
-                    title: "EUR to USD"
-                },
-                {
-                    proName: "BITSTAMP:BTCUSD",
-                    title: "Bitcoin"
-                },
-                {
-                    proName: "BITSTAMP:ETHUSD",
-                    title: "Ethereum"
-                }
-            ],
-            showSymbolLogo: true,
-            isTransparent: true,
-            displayMode: "adaptive",
-            colorTheme: "dark",
-            locale: "en"
-        });
-
-        if (widgetContainerRef.current) {
-            widgetContainerRef.current.appendChild(script);
-        }
-    }, []);
+ 
 
     return (
         <>
-            <div className="tradingview-widget-container" ref={widgetContainerRef}>
-                <div className="tradingview-widget-container__widget"></div>
-                <div className="tradingview-widget-copyright">
-                    <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-                        
-                    </a>
-                </div>
-            </div>
+           
             <div className="font-serif flex items-center justify-between p-4 z-[100] w-full absolute bg-black-800 h-[90px]">
                 <h1 className="text-white text-4xl font-bold cursor-pointer pl-5">DLQF</h1>
                 <div className="m-5">
