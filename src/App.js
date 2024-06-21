1e947510a2c8c87dfe1fcc8f25e28cdc5bb34f2a
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-06-21 07:19:19 +0530

    logo addded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from'./Components/Navbar';
+import Navbar from './Components/Navbar';
 import Projects from './Components/Pages/Projects';
 import Home from './Components/Pages/Home';
 import Apply from "./Components/Pages/Apply";
@@ -12,25 +12,26 @@ import React from "react";
 
 function App() {
   return (
-    <>
-        
-        <Navbar />
-        <div className="z-20">
-            <TradingViewTicker />
+      <>
 
-        </div>
+          <Navbar/>
+          <div className="z-20">
+              <TradingViewTicker/>
+
+          </div>
 
 
-        <Routes >
-            <Route path="/" element={<Home />} />
-            <Route path="/People" element = {<People />} />
-            <Route path="/Apply" element = {<Apply />} />
-            <Route path="/Projects"  element = {<Projects />} />
-            <Route path="/About"  element = {<About />} />
-            <Route path='/Contact' element = {<Contact />} />
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/People" element={<People/>}/>
+              <Route path="/Apply" element={<Apply/>}/>
+              <Route path="/Projects" element={<Projects/>}/>
+              <Route path="/About" element={<About/>}/>
+              <Route path='/Contact' element={<Contact/>}/>
 
-        </Routes>
-    </>
+          </Routes>
+
+      </>
   );
 }
 
