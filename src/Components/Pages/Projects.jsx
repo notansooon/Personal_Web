d579bcabb3fa08e1cbe526523fe5cbf2b22ab35a
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-06-21 06:15:53 +0530

    Update Projects.jsx

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -17,15 +17,13 @@ function Projects() {
 
       <div className="relative min-h-screen text-white font-serif overflow-hidden">
         {/* Gemini Effect Background */}
-        <div className="fixed inset-0 z-1">
-          <GoogleGeminiEffectDemo />
-        </div>
+
 
         {/* Content Overlay */}
         <div className="relative z-10 container mx-auto px-4 md:px-10 lg:px-20 xl:px-40 pt-60 space-y-16">
           <section className="bg-opacity-50 bg-gray-900 p-6 rounded-lg shadow-lg">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl mb-6">Algorithmic Trading</h2>
-            <div className="text-lg md:text-xl lg:text-2xl space-y-4">
+            <h2 className="text-3xl md:text-5xl lg:text-5xl mb-6">Algorithmic Trading</h2>
+            <div className="text-lg md:text-xl lg:text-xl space-y-4">
               <p>
                 Within the dataism research lab, we've established a dedicated team focused on the development and refinement of sophisticated algorithmic trading strategies. Our approach revolves around leveraging advanced statistical inference models to uncover meaningful patterns and insights within financial data. These models serve as the cornerstone of our methodology, enabling us to extract actionable signals from the vast amount of information available in the markets.
               </p>
@@ -39,8 +37,8 @@ function Projects() {
           </section>
 
           <section className="bg-opacity-50 bg-gray-900 p-6 rounded-lg shadow-lg">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl">Skill Vs Luck Replication Study</h2>
-            <p className="italic mb-6 text-lg md:text-xl lg:text-2xl">Alex Jaimes, Ryan Bradley, Ben Cummings</p>
+            <h2 className="text-3xl md:text-5xl lg:text-5xl">Skill Vs Luck Replication Study</h2>
+            <p className="italic mb-6 text-lg md:text-xl lg:text-xl">Alex Jaimes, Ryan Bradley, Ben Cummings</p>
             <div className="text-lg md:text-xl lg:text-2xl space-y-4">
               <p>
                 The Dataism Lab is currently exploring the possibility of replicating a seminal study published in 2005 by Cuthbertson, Nitzsche, and O’Sullivan. This study delves into the intriguing question of whether the performance of individual UK mutual funds can be attributed to skill or luck. Using statistical bootstrapping techniques, the authors found that while most funds' performance could be chalked up to luck, there existed a small subset with "bad skill" and an even smaller subset with "good skill". The paper shed light on the disproportionate impact of extreme funds on overall performance analysis.
