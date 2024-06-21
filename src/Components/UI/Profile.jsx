012520d2ac421ee4ceeddae074a8592e92af06e4
notansooon <ansonjiang043@gmail.com>
2024-06-20 23:06:42 -0400

    .

--- src/Components/UI/Profile.jsx
+++ src/Components/UI/Profile.jsx
@@ -19,9 +19,21 @@ export const Profile = ({user}) => {
 
 
   return (
-    <div className=" bg-white p-2 pb-[200px] w-[300px] shadow-lg mb-12 ml-10 mt-10">
-      <img className='m-auto' src='https://dataismlab.com/author/ben-cummings/avatar_hu546a42ec33fce78256da8ecdd017fc89_13394_270x270_fill_q75_lanczos_center.jpeg'/>
+    <div className="relative bg-white p-2 pb-[200px] w-[500px] shadow-lg mb-12 ml-10 mt-[250px] ">
+
+      <div className='absolute top-[-150px] left-[30%] transform '>
+        <img className='m-auto object-fill w-[200px] h-[200px]' style={{ borderRadius: '50%' }} src='https://dataismlab.com/author/ben-cummings/avatar_hu546a42ec33fce78256da8ecdd017fc89_13394_270x270_fill_q75_lanczos_center.jpeg'/>
+      </div>
+
+
+
+      <div className='text-black'>
+
+      <p className='text-4xl text-center mt-20'>Ben Cumming</p>
+      </div>
+      <div>
       <p className='mt-3 p-2'>{truncatedDescription} </p>
+      </div>
     </div>
   )
 }
