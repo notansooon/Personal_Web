a089b05b5aefbe526c578f1d25346dee76a61f1d
notansooon <ansonjiang043@gmail.com>
2024-07-16 00:53:40 -0400

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,7 +18,8 @@ const Navbar = () => {
     ];
 
     return (
-        <div className='bg-transparent flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
+        <div className='bg-transparent flex justify-between items-center h-24 max-w-[100%] mx-auto px-4 text-black'>
+            
             <h1 className='w-full text-3xl font-bold text-black'>REACT.</h1>
             <ul className='hidden md:flex'>
                 {navItems.map(item => (
@@ -27,9 +28,13 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+
+
             <div onClick={handleNav} className='block md:hidden'>
                 {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
+
+
             <ul className={nav ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r text-white border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
                 {navItems.map(item => (
@@ -38,6 +43,9 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+
+
+            
         </div>
     );
 };
