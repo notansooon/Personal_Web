e90635926023a069933c0d75ab0840b5ebb05c45
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-07-20 03:19:03 +0530

    .

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,6 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import ProjectComp from '../Components/ProjectComp';
 import { isVisible } from '@testing-library/user-event/dist/utils';
 import { animate } from 'framer-motion';
+import {Typography} from "@material-tailwind/react";
 
 function Projects() {
   const {ref: myRef, inView: visible} = useInView({triggerOnce: true, threshold: 1})
@@ -23,32 +24,32 @@ function Projects() {
       
       <div className='relative flex flex-col justify-center items-center p-8'>
 
-        <header className='relative h-[400px]  w-full overflow-hidden '>
-
-          <div className='absolute top-[5%] p-4 md:p-8 text-black'>
-            <h1 className='text-3xl md:text-5xl'>Project Blogs</h1>
-            <p className='text-5xl mt-6 ml-12'>Where Ambition Begins...</p>
+        <header className="bg-white p-8">
+          <div className="w-w-full container mx-auto pt-12 lg:pb-16 pb-8 text-center">
+            <Typography
+                variant="h1"
+                color="blue-gray"
+                className="mx-auto w-full text-3xl !leading-tight lg:max-w-3xl lg:text-5xl"
+            >
+              Our company mission is to lead the web development.
+            </Typography>
+            <Typography
+                variant="lead"
+                className="mx-auto lg:mt-8 mt-4 lg:mb-12 mb-6 !text-gray-500 lg:w-8/12"
+            >
+              The time is now for it to be okay to be great. People in this world
+              shun people for being great. For being a bright color.
+            </Typography>
           </div>
-
-
+          <div className="container mx-auto">
+          </div>
         </header>
-
-
-        <div className='w-screen h-28 bg-gray-600 text-3xl text-white'>
-          <ul className='flex flex-row justify-around align-middle items-center'>
-            <li>Algo</li>
-            <li>Client</li>
-            <li>Academic</li>
-          </ul>
-
-        </div>
-
 
         <div className='w-screen max-w-6xl mt-10  '>
 
 
           <div className='text-3xl  '>
-          <h1 ref={cRef}> 
+            <h1 ref={cRef}>
               <span className={`${Avisible ? 'render' : ''}`} data-text='Algorithmic Development'>
                 Algorithmic Development
               </span>
