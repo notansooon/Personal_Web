1aff71a2d0b0d234e977803d4560303352a37985
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-07-20 04:33:21 +0530

    Update People.jsx

--- src/Pages/People.jsx
+++ src/Pages/People.jsx
@@ -1,102 +1,101 @@
-import React from 'react'
-import Ben from '../ProfilePicture/ben.jpg'
-import Ali from '../ProfilePicture/ali.jpg'
-import Roshan from '../ProfilePicture/roshan.jpg'
-import Ryan from '../ProfilePicture/ryan.jpg'
-import Raseen from '../ProfilePicture/Raseen.jpg'
-import Gaurav from '../ProfilePicture/Gaurav.jpg'
-import Alex from '../ProfilePicture/alex.jpg'
-import Paul from '../ProfilePicture/paul.jpg'
-import Unknown from '../ProfilePicture/unknown.jpg'
-import Ben_f from '../ProfilePicture/ben_f.jpg'
-import Team from '../Components/Profile'
-import { HeroTextTeam } from '../Components/Profile'
-import {motion} from 'framer-motion'
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    IconButton,
+    Typography,
+} from "@material-tailwind/react";
+import { UserGroupIcon } from "@heroicons/react/24/solid";
 
-
-
-
-
-
-
-const People = () => {
+function TeamCard({ img, name, title, desc }) {
     return (
-        <>
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="contain"
-            >
-                <div className="w-full">
-                    
-
-                    <div className=" top-0 overflow-hidden pt-10 pb-12 lg:pt-[50px] lg:pb-[90px] bg-transparent dark:bg-dark">
-                        <div className='flex justify-end '>
-                            <HeroTextTeam />
-
-                        </div>
-                    </div>
-
-                    <section class="bg-white dark:bg-gray-900">
-    <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">our team</h1>
-
-        <p class="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
-            Each member brings unique skills and perspectives, working collaboratively to achieve excellence. Get to know the people behind our success and discover the stories that inspire our journey.
-        </p>
-
-        <div class="flex items-center justify-center">
-            <div class="flex items-center p-1 border border-blue-600 dark:border-blue-400 rounded-xl">
-                <button class="px-4 py-2 text-sm font-medium text-white capitalize bg-blue-600 md:py-3 rounded-xl md:px-12">design</button>
-                <button class="px-4 py-2 mx-4 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:mx-8 md:px-12">development</button>
-                <button class="px-4 py-2 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:px-12">marketing</button>
-            </div>
-        </div>
-
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-3">
-            <div className="flex flex-col items-center size-[50%]">
-                <img className="object-cover w-full rounded-xl aspect-square" src={Ali} />
-
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Name</h1>
-
-                <p class="mt-2 text-gray-500 capitalize dark:text-gray-300">Yes</p>
-
-                
-            </div>
-
-            <div class="flex flex-col items-center size-5/12">
-                <img class="object-cover w-full rounded-xl aspect-square" src={Ben} />
-
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Name</h1>
-
-                <p class="mt-2 text-gray-500 capitalize dark:text-gray-300">Yes</p>
-
-                
-            </div>
-
-            <div class="flex flex-col items-center size-5/12">
-                <img class="object-cover w-full rounded-xl aspect-square" src={Raseen} />
-
-                <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Bob Rob</h1>
-
-                <p class="mt-2 text-gray-500 capitalize dark:text-gray-300"></p>
-
-                
-            </div>
-        </div>
-    </div>
-</section>
-
-    
-
+        <Card color="transparent" shadow={false}>
+            <CardHeader shadow={false} floated={false} className="!m-0 w-40">
+                <img
+                    src={img}
+                    alt={name}
+                    className="h-full w-full object-cover object-center"
+                />
+            </CardHeader>
+            <CardBody className="!pl-0 text-left">
+                <Typography variant="h5" color="blue-gray" className="font-medium">
+                    {name}
+                </Typography>
+                <Typography className="mb-2 font-bold !text-gray-500 text-lg">
+                    {title}
+                </Typography>
+                <Typography className="mb-4 max-w-xs !font-normal !text-gray-500">
+                    {desc}
+                </Typography>
+                <div className="flex items-center gap-1.5">
+                    <IconButton variant="text" color="gray">
+                        <i className="fa-brands fa-twitter text-lg" />
+                    </IconButton>
+                    <IconButton variant="text" color="gray">
+                        <i className="fa-brands fa-linkedin text-lg" />
+                    </IconButton>
+                    <IconButton variant="text" color="gray">
+                        <i className="fa-brands fa-dribbble text-lg" />
+                    </IconButton>
                 </div>
-
-            </motion.div>
-
-
-        </>
+            </CardBody>
+        </Card>
     );
 }
 
-export default People
+const members = [
+    {
+        img: `https://www.material-tailwind.com/img/avatar1.jpg`,
+        name: "Ryan Samuel",
+        title: "Co-Founder",
+        desc: "Artist is a term applied to a person who engages in an activity deemed to be an art.",
+    },
+    {
+        img: `https://www.material-tailwind.com/img/avatar2.jpg`,
+        name: "Jordan Michael",
+        title: "Front-End Developer",
+        desc: "And I love you like Kanye loves Kanye. We need to restart the human foundation.",
+    },
+    {
+        img: `https://www.material-tailwind.com/img/avatar5.jpg`,
+        name: "Nora Hazel",
+        title: "UI/UX Designer",
+        desc: "If you have the opportunity to play this game of life you need to appreciate every moment.",
+    },
+];
+
+export function People() {
+    return (
+        <section className="py-8 px-8 lg:py-28">
+            <div className="container mx-auto">
+                <div className="mb-10 lg:mb-20">
+                    <IconButton size="lg">
+                        <UserGroupIcon className="h-6 w-6" />
+                    </IconButton>
+                    <Typography
+                        variant="h2"
+                        color="blue-gray"
+                        className="mt-6 mb-3 text-3xl lg:text-4xl"
+                    >
+                        The Executive Team
+                    </Typography>
+                    <Typography
+                        variant="lead"
+                        className="font-normal !text-gray-500 max-w-xl"
+                    >
+                        We&apos;re constantly trying to express ourselves and actualize our
+                        dreams. If you have the opportunity to play this game of life you
+                        need to appreciate every moment.
+                    </Typography>
+                </div>
+                <div className="col-span-2 grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+                    {members.map((props, key) => (
+                        <TeamCard key={key} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default People;
