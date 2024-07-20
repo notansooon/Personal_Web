05164ce6ac9cb7763c7516fa57bb59e1c1643a31
notansooon <ansonjiang043@gmail.com>
2024-07-20 17:38:11 -0400

    u

--- src/Pages/People.jsx
+++ src/Pages/People.jsx
@@ -179,7 +179,7 @@ function TeamCard({ img, name, title, desc }) {
 export function People() {
 
    
-
+    
     const scrollHandler = (event) => {
 
         const value = parseInt(event.target.getAttribute('data-scroll'));
@@ -199,7 +199,7 @@ export function People() {
         <section className="py-8 px-8 lg:py-28">
 
 
-            <div className="container  px-6 py-10 mx-auto">
+            <div className="container px-6 py-10 mx-auto">
                 <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">our team</h1>
 
                 <p className="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
@@ -208,7 +208,7 @@ export function People() {
 
                 <div className="flex items-center justify-center">
                     <div className="flex items-center p-1 border border-blue-600 dark:border-blue-400 rounded-xl">
-                        <button className="px-4 py-2 text-sm font-medium text-white capitalize bg-blue-600 md:py-3 rounded-xl md:px-12" data-scroll='400px' onClick={scrollHandler}>Executive</button>
+                        <button className="px-4 py-2 mx-4 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:mx-8 md:px-12" data-scroll='400px' onClick={scrollHandler}>Executive</button>
                         <button className="px-4 py-2 mx-4 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:mx-8 md:px-12" data-scroll='1000px' onClick={scrollHandler}>Algorithmic </button>
                         <button className="px-4 py-2 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:px-12" data-scroll='1600px' onClick={scrollHandler}>Client Servies</button>
                         <button className="px-4 py-2 text-sm font-medium text-blue-600 capitalize transition-colors duration-300 md:py-3 dark:text-blue-400 dark:hover:text-white focus:outline-none hover:bg-blue-600 hover:text-white rounded-xl md:px-12" data-scroll='2200px' onClick={scrollHandler}>Academic Researcher</button>
