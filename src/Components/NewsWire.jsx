34243c647057c700c30d5212772c8b4de45605a8
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-07-20 07:01:11 +0530

    Blog Update

--- src/Components/NewsWire.jsx
+++ src/Components/NewsWire.jsx
@@ -1,61 +1,70 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore,{Autoplay} from "swiper";
-
-import 'swiper/css';
-import {color} from "framer-motion";
 
 const NewsWire = () => {
+    const blogPosts = [
+        {
+            category: 'Business',
+            title: 'Preline becomes an official Instagram Marketing Partner',
+            description: 'Great news we\'re eager to share.',
+            imageUrl: 'https://images.unsplash.com/photo-1664574654529-b60630f33fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+            authorImageUrl: 'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80',
+            authorName: 'Aaron Larsson',
+            date: 'Feb 15, 2021',
+        },
+        {
+            category: 'Announcements',
+            title: 'Announcing a free plan for small teams',
+            description: 'At Wake, our mission has always been focused on bringing openness.',
+            imageUrl: 'https://images.unsplash.com/photo-1669824774762-65ddf29bee56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+            authorImageUrl: 'https://images.unsplash.com/photo-1669720229052-89cda125fc3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80',
+            authorName: 'Hanna Wolfe',
+            date: 'Feb 4, 2021',
+        },
+    ];
 
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            autoplay={{delay: 2000}}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide>{Slide()}</SwiperSlide>
-            <SwiperSlide>{Slide()}</SwiperSlide>
-            <SwiperSlide>{Slide()}</SwiperSlide>
-            <SwiperSlide>{Slide()}</SwiperSlide>
-            ...
-        </Swiper>
+        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <div className="grid lg:grid-cols-2 gap-6">
+                {blogPosts.map((post, index) => (
+                    <a key={index} className="group sm:flex rounded-xl" href="#">
+                        <div className="flex-shrink-0 relative rounded-xl overflow-hidden h-[200px] sm:w-[250px] sm:h-[350px] w-full">
+                            <img className="size-full absolute top-0 start-0 object-cover" src={post.imageUrl} alt="Image Description" />
+                        </div>
+                        <div className="grow">
+                            <div className="p-4 flex flex-col h-full sm:p-6">
+                                <div className="mb-3">
+                                    <p className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-gray-100 text-gray-800 dark:bg-neutral-800 dark:text-neutral-200">
+                                        {post.category}
+                                    </p>
+                                </div>
+                                <h3 className="text-lg sm:text-2xl font-semibold text-gray-800 group-hover:text-blue-600 dark:text-neutral-300 dark:group-hover:text-white">
+                                    {post.title}
+                                </h3>
+                                <p className="mt-2 text-gray-600 dark:text-neutral-400">
+                                    {post.description}
+                                </p>
+                                <div className="mt-5 sm:mt-auto">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0">
+                                            <img className="size-[46px] rounded-full" src={post.authorImageUrl} alt="Image Description" />
+                                        </div>
+                                        <div className="ms-2.5 sm:ms-4">
+                                            <h4 className="font-semibold text-gray-800 dark:text-neutral-200">
+                                                {post.authorName}
+                                            </h4>
+                                            <p className="text-xs text-gray-500 dark:text-neutral-500">
+                                                {post.date}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </div>
     );
 };
 
-function Slide(){
-    return (
-        <div className="grid md:grid-cols-2">
-            <div className="image">
-                <img alt=""/>
-
-            </div>
-            <div className="info flex justify-center flex-col">
-                <div className="cat">
-                    <p className="text-gray-800 hover:text-gray-600"> date</p>
-
-                </div>
-                <div className="title">
-                    <p className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600"> Title</p>
-                </div>
-                <p className="text-gray-500 py-3">
-                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
-                    text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
-                </p>
-
-            </div>
-        </div>
-    )
-}
-
-
-
-
-
-
-
 export default NewsWire;
-
-
-
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '../utils/home.css';
-import Sponsor from "../Components/Sponsor";
-import { FileUpload } from "../Components/forms";
-import Subscribe from "../Components/Newsletter";
+
+
+
+
 import Placement from '../Components/Placement';
 import NewsWire from '../Components/NewsWire';
 import data from '../assets/data/home.json';
 import ContactUs from './Contact';
-import { motion } from 'framer-motion';
 import useScrollAnimation from '../Components/UI/Custom-scroll-hook';
 import HomeHero from "../Components/Home-Hero"; // Adjust the path according to your project structure
 
@@ -21,11 +21,6 @@ const Home = () => {
             .catch(error => console.error('Error fetching content:', error));
     }, []);
 
-    const animationVariants = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0 },
-    };
-
     const [sectionRef1, controls1] = useScrollAnimation();
     const [sectionRef2, controls2] = useScrollAnimation();
     const [sectionRef3, controls3] = useScrollAnimation();
@@ -36,83 +31,50 @@ const Home = () => {
 
     return (
         <>
-            <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="contain"
-            >
+            <div className="contain fadeInUp">
                 <HomeHero/>
 
-                <motion.section
+                <section
                     ref={sectionRef1}
-                    initial="hidden"
-                    animate={controls1}
-                    variants={animationVariants}
-                    transition={{ duration: 0.5 }}
-                    className="text-center mb-16 px-4 md:px-16 lg:px-24"
+                    className="text-center mb-16 px-4 md:px-16 lg:px-24 fadeInUp"
                 >
                     <h2 className="text-3xl md:text-4xl font-semibold text-gray-900">Pioneering Excellence at Dataism Laboratory</h2>
                     <p className="mt-4 text-lg md:text-xl text-gray-700">Explore our diverse areas of research</p>
                     <div className="grid lg:grid-cols-3 gap-10 mt-8">
-                        <motion.div
-                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4"
+                        <div
+                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4 fadeInUp"
                             style={{ backgroundImage: 'url(/path/to/research-ai.jpg)' }}
-                            variants={animationVariants}
                         >
                             <div className="bg-black bg-opacity-50 p-4 w-full">
                                 <h3 className="text-2xl font-semibold text-white">Innovative Financial Technologies</h3>
                                 <p className="mt-2 text-gray-200">Harnessing the power of AI, Machine Learning, and Big Data Analytics, we develop state-of-the-art financial models and tools. Our focus is on creating innovative technologies that transform how financial data is analyzed and utilized, driving advancements in trading strategies, risk management, and market predictions.</p>
                             </div>
-                        </motion.div>
-                        <motion.div
-                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4"
+                        </div>
+                        <div
+                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4 fadeInUp"
                             style={{ backgroundImage: 'url(/path/to/research-biotech.jpg)' }}
-                            variants={animationVariants}
                         >
                             <div className="bg-black bg-opacity-50 p-4 w-full">
                                 <h3 className="text-2xl font-semibold text-white">High-Performance Computing</h3>
                                 <p className="mt-2 text-gray-200">Leveraging powerful computational resources, we tackle complex financial problems with unprecedented speed and accuracy. Our high-performance computing capabilities enable us to run sophisticated simulations and analyze large datasets, providing deeper insights and more precise financial solutions.</p>
                             </div>
-                        </motion.div>
-                        <motion.div
-                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4"
+                        </div>
+                        <div
+                            className="relative flex items-end bg-cover bg-center bg-no-repeat h-[450px] p-4 fadeInUp"
                             style={{ backgroundImage: 'url(/path/to/research-quantum.jpg)' }}
-                            variants={animationVariants}
                         >
                             <div className="bg-black bg-opacity-50 p-4 w-full">
                                 <h3 className="text-2xl font-semibold text-white">Transformative Research</h3>
-                                <p className="mt-2 text-gray-200">Our interdisciplinary team conducts pioneering research that bridges theoretical concepts with practical applications. By continuously pushing the boundaries of financial science, we aim to set new standards in quantitative finance and econometrics, contributing to the evolution of the financial industry.</p>
+                                <p className="mt-2 text-gray-200">Our interdisciplinary team conducts pioneering
+                                    research that bridges theoretical concepts with practical applications. By continuously pushing the boundaries of financial science, we aim to set new standards in quantitative finance and econometrics, contributing to the evolution of the financial industry.</p>
                             </div>
-                        </motion.div>
-                    </div>
-                </motion.section>
-
-                {/* Mission Section */}
-                <motion.section
-                    ref={sectionRef2}
-                    initial="hidden"
-                    animate={controls2}
-                    variants={animationVariants}
-                    transition={{ duration: 0.5 }}
-                    className="relative mb-16 flex flex-col lg:flex-row bg-cover bg-center bg-no-repeat bg-gray-300 bg-opacity-75 px-4 md:px-16 lg:px-24 py-14"
-                    style={{ backgroundImage: 'url(/path/to/mission-image.jpg)' }}
-                >
-                    <div className="lg:w-3/4">
-                        <h2 className="text-4xl font-semibold p-5 text-gray-600">Our Mission</h2>
-                        <div className="mt-4 text-lg md:text-xl text-gray-600">
-                            <p>Our mission is to conduct pioneering research that advances scientific understanding and technological innovation. We strive to create a collaborative environment that fosters creativity and excellence.</p>
                         </div>
                     </div>
-                </motion.section>
+                </section>
 
-                <motion.section
+                <section
                     ref={sectionRef3}
-                    initial="hidden"
-                    animate={controls3}
-                    variants={animationVariants}
-                    transition={{ duration: 0.5 }}
-                    className="relative mb-16 flex flex-col lg:flex-row bg-cover bg-center bg-no-repeat"
+                    className="relative mb-16 flex flex-col lg:flex-row bg-cover bg-center bg-no-repeat fadeInUp"
                     style={{ backgroundImage: 'url(/path/to/newswire-image.jpg)' }}
                 >
                     <div className="contain px-4 md:px-16 lg:px-24 py-14">
@@ -126,10 +88,10 @@ const Home = () => {
                                 </div>
                             </div>
 
-                            {/*<div className="mt-14 space-y-10">
+                            <div className="mt-14 space-y-10">
                                 <NewsWire />
-                                <Subscribe />
-                            </div>*/}
+
+                            </div>
                         </div>
 
                         <Placement/>
@@ -141,20 +103,17 @@ const Home = () => {
                         </div>
 
                         <section className="text-center">
-                        
-
-
-                        < ContactUs />
-                        
+                            <ContactUs />
                         </section>
                     </div>
-                </motion.section>
-            </motion.div>
+                </section>
+            </div>
         </>
     );
 };
 
 export default Home;
+
 
 
 
