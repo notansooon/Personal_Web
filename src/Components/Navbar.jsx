f2422f4eca7c08c715a0e19a9be4cc79b1bf559f
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-07-21 11:14:51 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,13 +17,13 @@ const Navbar = () => {
                         <Link to="/" className="text-xl font-bold text-blue-950">Dataism Lab</Link>
                     </div>
                     <div className="hidden md:flex space-x-8">
-                        <Link to="/" className="text-gray-700 hover:text-blue-950">Home</Link>
-                        <Link to="/about" className="text-gray-700 hover:text-blue-950">About</Link>
-                        <Link to="/projects" className="text-gray-700 hover:text-blue-950">Project</Link>
-                        <Link to="/Sectors" className="text-gray-700 hover:text-blue-950">Sectors</Link>
-                        <Link to="/apply" className="text-gray-700 hover:text-blue-950">Apply</Link>
-                        <Link to="/contact" className="text-gray-700 hover:text-blue-950">Contact</Link>
-                        <Link to="/people" className="text-gray-700 hover:text-blue-950">People</Link>
+                        <Link to="/" className="text-gray-800 hover:text-blue-950">Home</Link>
+                        <Link to="/about" className="text-gray-800 hover:text-blue-950">About</Link>
+                        <Link to="/projects" className="text-gray-800 hover:text-blue-950">Project</Link>
+                        <Link to="/Sectors" className="text-gray-800 hover:text-blue-950">Sectors</Link>
+                        <Link to="/apply" className="text-gray-800 hover:text-blue-950">Apply</Link>
+                        <Link to="/contact" className="text-gray-800 hover:text-blue-950">Contact</Link>
+                        <Link to="/people" className="text-gray-800 hover:text-blue-950">People</Link>
                     </div>
                     <div className="md:hidden">
                         <button onClick={toggleMenu} className="focus:outline-none">
@@ -39,13 +39,13 @@ const Navbar = () => {
             {isOpen && (
                 <div className="md:hidden">
                     <div className="flex flex-col space-y-1 px-4 py-2">
-                        <Link to="/" className="text-gray-700 hover:text-blue-600">Home</Link>
-                        <Link to="/about" className="text-gray-700 hover:text-blue-600">About</Link>
-                        <Link to="/projects" className="text-gray-700 hover:text-blue-600">Project</Link>
-                        <Link to="/apply" className="text-gray-700 hover:text-blue-600">Apply</Link>
-                        <Link to="/Sectors" className="text-gray-700 hover:text-blue-600">Sectors</Link>
-                        <Link to="/contact" className="text-gray-700 hover:text-blue-600">Contact</Link>
-                        <Link to="/people" className="text-gray-700 hover:text-blue-600">People</Link>
+                        <Link to="/" className="text-gray-800 hover:text-blue-950">Home</Link>
+                        <Link to="/about" className="text-gray-800 hover:text-blue-950">About</Link>
+                        <Link to="/projects" className="text-gray-800 hover:text-blue-950">Project</Link>
+                        <Link to="/apply" className="text-gray-800 hover:text-blue-950">Apply</Link>
+                        <Link to="/Sectors" className="text-gray-800 hover:text-blue-950">Sectors</Link>
+                        <Link to="/contact" className="text-gray-800 hover:text-blue-950">Contact</Link>
+                        <Link to="/people" className="text-gray-800 hover:text-blue-950">People</Link>
 
                     </div>
                 </div>
