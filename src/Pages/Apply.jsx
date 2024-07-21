0ed807603612f27552321e0abc84333e844d7a94
notansooon <ansonjiang043@gmail.com>
2024-07-21 03:01:56 -0400

    d

--- src/Pages/Apply.jsx
+++ src/Pages/Apply.jsx
@@ -5,7 +5,15 @@ const Apply = () => {
     return (
         <div className="min-h-screen bg-gray-100 py-8">
             <div className="container mx-auto px-4">
-                <h1 className="text-3xl font-bold mb-8 text-center">Employment Opportunities</h1>
+            <header className='relative h-[400px]  w-full overflow-hidden '>
+
+                <div className='absolute top-[5%] p-4 md:p-8 text-black'>
+                    <h1 className='text-3xl md:text-5xl'>Discover Opportunities</h1>
+                    <p className='text-5xl mt-6 ml-12'>Find, Apply, Innovate</p>
+                </div>
+
+
+            </header>
                 <JobList />
             </div>
         </div>
