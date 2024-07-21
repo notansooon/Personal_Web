ef169166f6d33a47c6b52553f34007c74c291252
notansooon <ansonjiang043@gmail.com>
2024-07-21 14:34:45 -0400

    d

--- src/Components/NewsWireCarousel.jsx
+++ src/Components/NewsWireCarousel.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/pagination';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation'
+import '../Components/utils/styles.css'
 
 
 SwiperCore.use([Pagination]);
@@ -16,34 +17,29 @@ SwiperCore.use([Navigation]);
 
 const NewsWireCarousel = ()=>{
     return (
-        <section className="newsWireCarousel py-16">
-            <div className="container mx-auto md:px-20">
-                <h1 className="font-bold text-4xl pb-12 text-center -mb-32">Trending</h1>
-                <Swiper
-                    spaceBetween={50}
-                    slidesPerView={1}
-                    pagination={{clickable:true}}
-                    autoplay=
-                        {{
-                        delay:4000,
-                        disableOnInteraction:true,
-                        pauseOnMouseEnter:true
-                        }}
-                    navigation={{enabled:true, hideOnClick:true}}
-
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                >
-
-                    <SwiperSlide>{NewsWire}</SwiperSlide>
-                    <SwiperSlide>{NewsWire}</SwiperSlide>
-                    <SwiperSlide>{NewsWire}</SwiperSlide>
-                    <SwiperSlide>{NewsWire}</SwiperSlide>
-                    ...
-                </Swiper>
-            </div>
-        </section>
-
+        <div className="w-[100%] mx-auto px-4 md:px-20">
+            <h1 className="  container font-bold text-4xl pb-12 text-center">Trending</h1>
+            <Swiper
+                spaceBetween={20}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: true,
+                    pauseOnMouseEnter: true,
+                }}
+                navigation={{ enabled: true, hideOnClick: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                style={{ overflow: 'hidden' }}
+            >
+                <SwiperSlide><NewsWire /></SwiperSlide>
+                <SwiperSlide><NewsWire /></SwiperSlide>
+                <SwiperSlide><NewsWire /></SwiperSlide>
+                <SwiperSlide><NewsWire /></SwiperSlide>
+                
+            </Swiper>
+        </div>
     )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -38,17 +38,10 @@ const Home = () => {
                     <Sector clas></Sector>
                 </section>
 
-                <section
-                    className="relative mb-16 flex flex-col lg:flex-row bg-cover bg-center bg-no-repeat fadeInUp bg-gray-200">
-                    <div className="contain px-4 md:px-16 lg:px-24 py-14">
-                        <div className="space-y-10">
-                            <NewsWireCarousel />
+                
+                <NewsWireCarousel />
                             
-                        </div>
                         
-                    </div>
-                </section>
-
 
 
 
