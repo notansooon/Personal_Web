9fc45ebdac7e740cb9740aff331bbbd47a9b00b8
devnumber7 <53370234+devnumber7@users.noreply.github.com>
2024-07-21 23:47:33 +0530

    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,44 +14,22 @@ import Sector from "../Components/Sector";
 import {Typography} from "@material-tailwind/react";
 
 const Home = () => {
-
-    const animationVariants = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0 },
-    };
-
-    const [sectionRef1, controls1] = useScrollAnimation();
-    const [sectionRef2, controls2] = useScrollAnimation();
-    const [sectionRef3, controls3] = useScrollAnimation();
-
     return (
         <>
             <div className="contain fadeInUp">
-                <HomeHero/>
-
+                <HomeHero />
 
                 <section className='mt-32'>
                     <Typography variant='h3' className='text-center'>
                         The sectors that we work on
                     </Typography>
-                    <Sector clas></Sector>
+                    <Sector/>
                 </section>
 
-                <section
-                    className="relative mb-16 flex flex-col lg:flex-row bg-cover bg-center bg-no-repeat fadeInUp bg-gray-200">
-                    <div className="contain px-4 md:px-16 lg:px-24 py-14">
-                        <div className="space-y-10">
-                            <NewsWireCarousel />
-                        </div>
-                        
-                    </div>
-                </section>
+                <NewsWireCarousel/>
 
-
-                
-
-                <div>
-                    <Placement/>
+                <div className="px-4 md:px-16 lg:px-24">
+                    <Placement />
                 </div>
             </div>
         </>
