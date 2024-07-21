4858d1775b1f098f64c8256683149eacbe854172
notansooon <ansonjiang043@gmail.com>
2024-07-21 14:42:53 -0400

    d

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,11 +26,7 @@ const Home = () => {
                     <Sector/>
                 </section>
 
-                <NewsWireCarousel/>
-
-                <div className="px-4 md:px-16 lg:px-24">
-                    <Placement />
-                </div>
+                
             </div>
         </>
     );
