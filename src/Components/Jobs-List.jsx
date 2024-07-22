95bd89f732c61e5e1b82391df1d0f0d09154913f
notansooon <ansonjiang043@gmail.com>
2024-07-22 02:04:50 -0400

    t

--- src/Components/Jobs-List.jsx
+++ src/Components/Jobs-List.jsx
@@ -4,7 +4,7 @@ import Job from './job';
 
 const JobList = () => {
     const jobs = [
-        { title: 'Head of Algorithmic Development',
+        { title: 'Senior Algorithmic Development',
             description: 'Are you passionate about developing cutting-edge trading algorithms and driving innovation ' +
                 'in the field of quantitative finance. Join our dynamic team at the Dataism Laboratory for Quantitative ' +
                 'Finance at Virginia Tech as the Head of Algorithmic Development. This role is open to both graduate and ' +
@@ -14,7 +14,35 @@ const JobList = () => {
                 'Lead and mentor a team of developers in creating AI and ML algorithms for financial forecasting and analysis.\n',
    
             status: 'Active',
-            pay: ' Not Paid'
+            pay: ' Not Paid',
+            responsibilities: [
+                'Oversee the development of state-of-the-art trading algorithms.', 
+                'Lead and mentor a team of developers in creating AI and ML algorithms for financial forecasting and analysis.', 
+                'Collaborate with cross-functional teams to design and implement robust algorithmic solutions.', 
+                'Assist the development team in debugging and optimizing algorithms to enhance performance and efficiency.',
+                'Conduct research to identify and incorporate the latest advancements in AI, ML, and quantitative finance into our projects.',
+                'Ensure the accuracy, reliability, and scalability of all developed algorithms.',
+                'Present findings and progress to stakeholders and contribute to strategic decision-making.'
+            ]
+
+        },
+        { title: 'Algorithmic Developer',
+            description: 'Are you enthusiastic about developing sophisticated trading algorithms and contributing to advancements in quantitative finance?' + 
+                ' The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented and motivated Algorithmic Developer to assist the Head of Algorithmic Development.' + 
+                ' This role is open to both graduate and undergraduate students pursuing degrees in Computational Modeling and Data Analytics (CMDA), Computer Science, Finance, Financial Technology, and related majors.'
+        ,
+            status: 'Not Hiring',
+            pay: ' Not Paid',
+            responsibilities: [
+                'Collaborate with the Head of Algorithmic Development and other team members to create, test, and implement AI and ML algorithms for financial forecasting and trading.',
+                'Assist in the debugging and optimization of algorithms to enhance their performance, reliability, and scalability.',
+                'Conduct research and stay updated with the latest advancements in AI, ML, and quantitative finance to integrate into ongoing projects.',
+                'Participate in code reviews and provide constructive feedback to ensure high-quality code development.',
+                'Analyze financial data and apply quantitative techniques to develop innovative trading strategies.',
+                'Contribute to the documentation of algorithmic solutions and processes.', 
+                'Assist in presenting research findings and project progress to stakeholders.'
+            ]
+
         },
         { title: 'Data Scientist',
             description: 'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented and motivated Data Scientist to join our Client Services Sector.' + 
@@ -22,7 +50,19 @@ const JobList = () => {
                 'If you are passionate about applying data science techniques to solve complex financial challenges, we want to hear from you.'
         ,
             status: 'Not Hiring',
-            pay: ' Not Paid'
+            pay: ' Not Paid',
+            responsibilities: [
+                'Collaborate with clients to understand their business needs and translate them into data-driven solutions.',
+                'Develop and implement machine learning models and algorithms for financial forecasting, risk management, and trading strategies.',
+                'Analyze large financial datasets to identify trends, patterns, and actionable insights.',
+                'Design and build data pipelines and automated workflows to streamline data processing and analysis.',
+                'Perform rigorous testing and validation of models to ensure accuracy, reliability, and scalability.',
+                'Communicate findings and recommendations to clients through detailed reports and presentations.',
+                'Work with cross-functional teams, including developers and financial analysts, to integrate data solutions into existing systems and processes.',
+                'Stay updated with the latest advancements in data science, AI, and finance to continually enhance the solutions provided to clients.'
+
+            ]
+
         },
         { title: 'UI/UX Designer',
             description: 'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented UI/UX Designer to join our Client Services Sector.' + 
@@ -30,7 +70,19 @@ const JobList = () => {
                 'If you have a passion for creating seamless user experiences and a keen eye for design, we would love to have you on our team.'
             ,
             status: 'Active',
-            pay: 'Paid'
+            pay: 'Paid',
+            responsibilities:[
+                'Collaborate with clients and internal teams to understand user requirements and translate them into effective UI/UX designs.',
+                'Design and prototype user interfaces for web and mobile applications that are visually appealing, user-friendly, and aligned with client needs.',
+                'Conduct user research and usability testing to gather insights and refine designs.',
+                'Create wireframes, storyboards, user flows, and interactive prototypes to communicate design ideas effectively.',
+                'Develop and maintain design systems and style guides to ensure consistency across all products.',
+                'Work closely with developers to ensure the implementation of designs is pixel-perfect and meets usability standards.',
+                'Stay updated with the latest UI/UX trends, techniques, and technologies to continuously improve design processes.',
+                'Produce high-quality visual designs, including layouts, color schemes, typography, and iconography.'
+
+            ]
+
         },
         { title: 'Graduate Researcher',
             description: 'Are you a graduate student driven by a passion for quantitative finance and eager to engage in high-impact research?' +
@@ -38,7 +90,18 @@ const JobList = () => {
                 'This role offers a platform to work on pioneering projects, collaborate with experts, and contribute to advancements in the field of quantitative finance.'
             ,
             status: 'Active',
-            pay: ' Not Paid'
+            pay: ' Not Paid',
+            responsibilities:[
+                'Lead and participate in research projects focused on developing and refining financial models and algorithms.',
+                'Conduct advanced data collection, cleaning, and analysis to support research objectives.',
+                'Collaborate with faculty members, postdoctoral researchers, and undergraduate assistants on interdisciplinary research initiatives.',
+                'Perform comprehensive literature reviews and synthesize findings to inform research direction.',
+                'Prepare and present research reports, papers, and presentations for academic journals and conferences.',
+                'Develop and optimize algorithms for financial forecasting, risk management, and trading strategies.',
+                'Contribute to the mentoring of undergraduate research assistants and foster a collaborative research environment.'
+
+
+            ]
         },
         { title: 'Undergraduate Research Assistant',
             description: 'Are you an undergraduate student passionate about quantitative finance and eager to contribute to cutting-edge research?' + 
@@ -46,7 +109,18 @@ const JobList = () => {
                 'This role offers a unique opportunity to work alongside experienced researchers and gain hands-on experience in the exciting field of quantitative finance.'
             ,
             status: 'Active',
-            pay: 'Paid'
+            pay: 'Paid',
+            responsibilities:[
+                
+                '- Assist in the development and implementation of financial models and algorithms.', 
+                '- Conduct data collection, cleaning, and analysis to support ongoing research projects.',
+                '- Collaborate with graduate students and faculty members on various research initiatives.',
+                '- Participate in literature reviews and summarize relevant academic papers and industry reports.',
+                '- Contribute to the preparation of research reports, presentations, and publications.',
+                '- Assist in the debugging and optimization of algorithms and models.',
+                '- Attend lab meetings and contribute to discussions on research progress and findings.'
+            ]  
+
             
         },
     ];
@@ -54,7 +128,7 @@ const JobList = () => {
     return (
         <div className="space-y-4">
             {jobs.map((job, index) => (
-                <Job key={index} title={job.title} description={job.description} status={job.status} pay={job.pay} />
+                <Job key={index} title={job.title} description={job.description} status={job.status} pay={job.pay} responsibilities={job.responsibilities}/>
             ))}
         </div>
     );
--- src/Components/job.jsx
+++ src/Components/job.jsx
@@ -1,25 +1,62 @@
 // Job.js
 import React from 'react';
+import { useState } from 'react';
 
-const Job = ({ title, description, status, pay }) => {
+const Job = ({ title, description, status, pay, responsibilities }) => {
+
+    const [Expanded, setExpanded] = useState(false);
+
+    const clickHandler = (event) => {
+        const exp = event.target.getAttribute('expand');
+        if (exp === 'data-expand') {
+            setExpanded(true);
+        } else {
+            setExpanded(false);
+        }
+    };
+
     return (
-        <div className={`p-4 border rounded-lg shadow-md ${status === 'Active' ? 'bg-green-100' : 'bg-red-100'}`}>
-            <h2 className="text-xl font-semibold">{title}</h2>
-            <p className="mt-2">{description}</p>
-            <div className='space-x-5'>
+        <div className={`p-4 border rounded-lg shadow-md ${status === 'Active' ? 'bg-green-100' : 'bg-red-100'}`} >
+            {Expanded ? (
+                <>
+                    <h2 className="text-xl font-semibold">{title}</h2>
+                    <p className="mt-2">{description}</p>
+                    <h1 className='text-sm font-bold mt-5'>Responsibilities</h1>
+                    <ul className="list-disc list-inside">
+                        {responsibilities.map((responsibility, index) => (
+                            <li key={index}>{responsibility}</li>
+                        ))}
+                    </ul>
+                    
 
-            <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${status === 'Active' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
-                {status === 'Active' ? 'Actively Hiring' : 'Not Hiring'}
-            </span>
+                    <div className='space-x-5'>
+                        <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${status === 'Active' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
+                            {status === 'Active' ? 'Actively Hiring' : 'Not Hiring'}
+                        </span>
+                        <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${pay === 'Paid' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
+                            {pay === 'Paid' ? 'Paid' : 'Not Paid'}
+                        </span>
+                    </div>
+                
+                </>
+            ) : (
+                <>
+                    <h2 className="text-xl font-semibold">{title}</h2>
+                    <p className="mt-2">{description}</p>
+                    <div className='space-x-5'>
+                        <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${status === 'Active' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
+                            {status === 'Active' ? 'Actively Hiring' : 'Not Hiring'}
+                        </span>
+                        <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${pay === 'Paid' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
+                            {pay === 'Paid' ? 'Paid' : 'Not Paid'}
+                        </span>
+                    </div>
+                </>
+            )}
 
-            <span className={`inline-block mt-4 px-3 py-1 rounded-full text-sm ${pay === 'Paid' ? 'bg-green-200 text-green-800' : 'bg-red-200 text-red-800'}`}>
-                {status === 'Paid' ? 'Paid' : 'Not Paid'}
-            </span>
-
-            </div>
-           
-
-
+                        <span className='inline-block mt-4 px-3 py-1 rounded-full text-sm bg-green-200 text-green-800' expand='data-expand' onClick={clickHandler}>
+                            Show More
+                        </span>
         </div>
     );
 };
