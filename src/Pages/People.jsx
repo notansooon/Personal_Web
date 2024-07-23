f0409574f402322f33b56f433b5e07e5753bd991
notansooon <ansonjiang043@gmail.com>
2024-07-23 02:29:15 -0400

    .

--- src/Pages/People.jsx
+++ src/Pages/People.jsx
@@ -192,62 +192,48 @@ export function People() {
     return (
         <section className="">
 
-<div className='text-white h-[550px] w-full relative'>
-      <div className='w-full h-full'>
+<div className='text-white h-svh-50 lg:h-[550px] surface:h-[950px] ipad:h-[700px]   md:h-[550px] sm:h-svh-60 w-full relative'>
+    <div className='w-full h-full'>
         <img className='absolute inset-0 h-full w-full object-cover' src={group} alt="Group" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        
         <div className='absolute top-[10%] ml-[8%] p-4 md:p-8'>
-          <Typography
-            variant="h1"
-            color="white"
-            className="text-4xl !leading-snug lg:text-5xl"
-          >
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString("Meet Our Team").start();
-              }}
-            />
-          </Typography>
+            <Typography
+                variant="h1"
+                color="white"
+                className="text-4xl !leading-snug lg:text-5xl"
+            >
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter.typeString("Meet Our Team").start();
+                    }}
+                />
+            </Typography>
         </div>
-      </div>
+    </div>
+</div>
+
+<div className="container px-6 py-10 mx-auto">
+    <div className="flex items-center justify-center p-2">
+        <div className="flex flex-wrap items-center justify-center p-1 border border-blue-950 rounded-xl">
+            <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='400px' onClick={scrollHandler}>Executive</button>
+
+            <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='1000px' onClick={scrollHandler}>Algorithmic</button>
+
+            <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='1600px' onClick={scrollHandler}>Client Services</button>
+
+            <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='2200px' onClick={scrollHandler}>Academic Researcher</button>
+        </div>
     </div>
 
-
-
-                    
-        
-
-        
-        
-
-                
-            <div className="container px-6 py-10 mx-auto ">
-                
-
-                
-                <div className="flex items-center justify-center p-2">
-                    <div className="flex flex-wrap items-center justify-center p-1 border border-blue-950 rounded-xl">
-                        <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='400px' onClick={scrollHandler}>Executive</button>
-
-                        <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='1000px' onClick={scrollHandler}>Algorithmic</button>
-
-                        <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='1600px' onClick={scrollHandler}>Client Services</button>
-
-                        <button className="px-4 py-2 mx-2 my-1 text-xs font-medium text-blue-950 capitalize transition-colors duration-300 md:text-sm md:py-3 md:mx-4 lg:mx-8 lg:px-12 focus:outline-none hover:bg-blue-950 hover:text-white rounded-xl" data-scroll='2200px' onClick={scrollHandler}>Academic Researcher</button>
-                    </div>
-                </div>
-
-                <p className="max-w-2xl mx-auto my-6 text-center text-gray-500">
-                    Each member brings unique skills and perspectives, working collaboratively to achieve excellence.
-                    Get to know the people behind our success and discover the stories that inspire our journey.
-                </p>
-
-            </div>
+    <p className="max-w-2xl mx-auto my-6 text-center text-gray-500">
+        Each member brings unique skills and perspectives, working collaboratively to achieve excellence.
+        Get to know the people behind our success and discover the stories that inspire our journey.
+    </p>
+</div>
 
             <div className="container ">
 
-    <div className="mb-10 lg:mb-20">
+    <div className="mb-10 lg:mb-20 responsive-margin-height responsive-margin-height-md">
         <Typography
             variant="h2"
             color="blue-gray"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
@@ -7,12 +9,54 @@ module.exports = {
     extend: {
       fontSize: {
         '2.5xl': '1.75rem', 
-         // Example size between 2xl (1.5rem) and 3xl (1.875rem)
-      }
+        
+      },
+      spacing: {
+        'svh-41': '41svh',
+        'svh-50': '50svh',
+        'svh-60': '60svh',
+        'svh-75': '75svh',
+        'svh-80': '80svh',
+        'svh-100': '100svh',
+        
+      },
+      screens: {
+        'xs': '320px',  
+        'ipad': '768px',
+        'surface': '900px', 
+        'ipadP': '1024px',  
+        'surface2': '540',    
+        
+      },
+
+
+
 
     },
   },
-  plugins: [require('tailwind-scrollbar-hide')],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+    plugin(function({ addBase }) {
+      addBase({
+        '@media (min-width: 300px) and (min-height: 800px)': {
+          '.responsive-margin-height': {
+            marginTop: '50px',
+          },
+        },
+        '@media (min-width: 400px) and (min-height: 800px)': {
+          '.responsive-margin-height-md': {
+            marginTop: '100px',
+          },
+        },
+        '@media (min-width: 768px) and (min-height: 800px)': {
+          '.responsive-ipad-height': {
+            height: '1050px',
+          },
+        },
+      
+      });
+    }),
+  ],
 }
 
 
