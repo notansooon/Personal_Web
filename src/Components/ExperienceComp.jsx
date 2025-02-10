4cef7e40ebd29dde2206c4a84db776a1dcf99bc1
notansooon <ansonjiang043@gmail.com>
2025-02-10 09:45:36 -0500

    Update

--- src/Components/ExperienceComp.jsx
+++ src/Components/ExperienceComp.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import '../Components/utils/experience.css'
 
-const ExperienceComp = ({ title, company, description }) => {
+const ExperienceComp = ({ title, company, description, number, img, date }) => {
 
            return (
                       <>
                       <div className="timeline">
-                                 <div className="contain">
-                                            <div className="textbox">
-                                            <h2>{title}</h2>
-                                            <h1>{company}</h1>
-                                            <p>{description}</p>
-                                            </div>
+                                 <div className={`${ number === 0 ? "right-container contain": "left-container contain"}`}>
+                                     <img src={img} className={`${number === 0 ? "image-right" : "image-left"}`}/>
+                                     <div className="textbox">
+
+                                     <h2 className="text-xl font-bold">{title}</h2>
+                                     <small>{date}</small>
+                                     <h1>{company}</h1>
+                                     <p>{description}</p>
+                                     </div>
                                             
                                  </div> 
 
--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -1,38 +1,47 @@
 import React from "react";
 import ExperienceComp from "../Components/ExperienceComp";
+import { number } from "prop-types";
+import NAVAGANT from "../Components/Images/NAVAGANT.png"
+import VT from "../Components/Images/VT.jpeg"
+import dataism from "../Components/Images/dataism.jpeg"
 
 
 const jobs = [
            { title: 'Developer',
                job: 'Barrett Capitol',
-               description: 'Are you passionate about developing cutting-edge trading algorithms and driving innovation ' +
-                   'in the field of quantitative finance. Join our dynamic team at the Dataism Laboratory for Quantitative ' +
-                   'Finance at Virginia Tech as the Head of Algorithmic Development. This role is open to both graduate and ' +
-                   'undergraduate students pursuing degrees in Computational Modeling and Data Analytics (CMDA), ' +
-                   'Computer Science, Finance, Financial Technology, and related majors.\n',
+               description: '',
+                number: 1,
+                img: {},
+                date:"02/2025 - Present"
+
    
            },
            { title: 'Data Scientist',
                job: 'NAVAGANT',
-               description: 'Are you enthusiastic about developing sophisticated trading algorithms and contributing to advancements in quantitative finance?' + 
-                   'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented and motivated Algorithmic Developer to assist the Head of Algorithmic Development.' + 
-                   'This role is open to both graduate and undergraduate students pursuing degrees in Computational Modeling and Data Analytics (CMDA), Computer Science, Finance, Financial Technology, and related majors.'
+               description: '',
+                number: 0,
+                img: NAVAGANT,
+                date:'09/2024 - 01/2025'
            ,
            },
            { title: 'Quant Researcher',
                job: 'Dataism At Quantitive Finance',
-               description: 'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented and motivated Data Scientist to join our Client Services Sector.' + 
-                   'In this role, you will work directly with investment banks to design, develop, and implement advanced data-driven solutions that enhance their strategic decision-making and operational efficiency.' +
-                   'If you are passionate about applying data science techniques to solve complex financial challenges, we want to hear from you.'
-           ,
+               description: 'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented and motivated Data Scie',
+                number: 1,
+                img: dataism,
+                date:'07/2024 - Present'
+           
            },
 
            { title: 'Software Developer',
                job: 'SMART @IDPRO VT',
                description: 'The Dataism Laboratory for Quantitative Finance at Virginia Tech is seeking a talented UI/UX Designer to join our Client Services Sector.' + 
                    'This role involves designing intuitive and engaging user interfaces for advanced financial applications used by leading investment banks.' + 
-                   'If you have a passion for creating seamless user experiences and a keen eye for design, we would love to have you on our team.'
-           ,
+                   'If you have a passion for creating seamless user experiences and a keen eye for design, we would love to have you on our team.',
+                number: 0,
+                img: VT,
+                date:'01/2024 - Present'
+           
            },
            
        ];
@@ -48,6 +57,9 @@ const Experience = () => {
                             title={index.title}
                             company={index.job}
                             description={index.description}
+                            number={index.number}
+                            img={index.img}
+                            date={index.date}
 
                         />
                       ))}
